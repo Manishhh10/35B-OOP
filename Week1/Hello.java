4a039e45e0fe6c2a45e6367647b7476b4491252a
manish <manishbhatt2121714@gmail.com>
2024-04-15 11:21:26 +0545

    completed

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -1,4 +1,3 @@
-
 public class Hello{
     public static void main(String[] args) {
         /* 1.Write a program that prints Hello World! to the console. Save the program as Hello.java.*/
---
+++ Week1/question2.java
@@ -0,0 +1,13 @@
+public class question2 {
+    public static void main(String[] args) {
+        /*2. Extend/Modify the above program to print two additional lines:
+        This is my first programI am on module ST4003CEMSystem.out.print (not println) and produces the same output.*/
+        System.out.println("Hello World!");
+        System.out.println("This is my first program");
+        System.out.println("I am on module ST4003CEM");
+
+        System.out.print("Hello World!");
+        System.out.print("This is my first program");
+        System.out.print("I am on module ST4003CEM");
+    }
+}
