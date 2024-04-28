be5e54b4addc209a27e0cfd6bf2cbc9bb50f41d6
manish <manishbhatt2121714@gmail.com>
2024-04-28 15:15:16 +0545

    If Else Class and Switch Case

---
+++ SwitchCase.java
@@ -0,0 +1,67 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        char charValue = 'b';
+        char intValue = 10;
+        switch (charValue) {
+            case 'a':
+                System.out.println("Apple");
+                break;
+            case 'b':
+            switch (intValue) {
+                case 10:
+                    System.out.println("Ten");
+                    break;
+                case 20:
+                    System.out.println("Twenty");
+                    break;
+            }
+                System.out.println("Ball");
+                break;
+            case 'c':
+                System.out.println("Cat");
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+        }
+        /* Task
+         * Use switch case for the following
+         * 0 - Sunday
+         * 1 - Monday
+         * 2 - Tuesday
+         * 3 - Wednesday
+         * 4 - Thursday
+         * 5 - Friday
+         * 6 - Saturday
+         * rest - Invalid day
+         */
+        int day = 4;
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+        
+            default:
+            System.out.println("Invalid");
+                break;
+        }
+    }
+}
